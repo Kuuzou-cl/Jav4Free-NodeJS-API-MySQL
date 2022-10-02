6122990bbef9492018f150b2bf9723d493ecd22a
Carlo Bernucci Gómez <carlobernucci@gmail.com>
2022-10-02 15:40:37 -0500

    fix maxpage and nextpage function

--- services/category.js
+++ services/category.js
@@ -21,7 +21,10 @@ async function getScenes(page = 1, name = '', order = 'desc') {
     const rows = await db.query(
         `SELECT * from Scene s join SceneCategory sc on s.id = sc.sceneId where sc.categoryId = (SELECT c.id from Category c WHERE c.name = '${name}') order by s.creation ${order} LIMIT ${offset},${config.listPerPageScenes}`
     );
-    const pagesData = helper.getCountPages(page,config.listPerPageScenes,rows.length);
+    const maxRows = await db.query(
+        `SELECT count(*) from Scene s join SceneCategory sc on s.id = sc.sceneId where sc.categoryId = (SELECT c.id from Category c WHERE c.name = '${name}')`
+    );
+    const pagesData = helper.getCountPages(page,config.listPerPageScenes,maxRows);
     const data = {Scenes : helper.emptyOrRows(rows)};
     const meta = { page : page, total: pagesData };
 
