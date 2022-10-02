2cbd3adebcd90d9b309628d5893db1f096762868
Carlo Bernucci Gómez <carlobernucci@gmail.com>
2022-10-02 15:17:43 -0500

    fix json categories

--- services/category.js
+++ services/category.js
@@ -21,7 +21,7 @@ async function getScenes(page = 1, name = '', order = 'desc') {
     const rows = await db.query(
         `SELECT * from Scene s join SceneCategory sc on s.id = sc.sceneId where sc.categoryId = (SELECT c.id from Category c WHERE c.name = '${name}') order by s.creation ${order} LIMIT ${offset},${config.listPerPageScenes}`
     );
-    const data = helper.emptyOrRows(rows);
+    const data = {Scenes : helper.emptyOrRows(rows)};
     const meta = { page };
 
     return {
