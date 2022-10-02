b406a8eeb95cfbf31e92944a2e2894be2f358660
Carlo Bernucci Gómez <carlobernucci@gmail.com>
2022-10-02 15:46:04 -0500

    fix

--- helper.js
+++ helper.js
@@ -13,7 +13,7 @@ function getCountPages(currentPage = 1, listPerPage, totalRows){
   if (totalRows % listPerPage == 0 ) {
     lastPage = totalRows / listPerPage;
   }else{
-    lastPage = totalRows / listPerPage + 1 - totalRows % listPerPage;
+    lastPage = ((totalRows / listPerPage) - (totalRows % listPerPage)) + 1;
   }
   return ({nextPage, lastPage});
 }
