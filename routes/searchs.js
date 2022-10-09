cd75b5b7768d82cabb520575e839a9c0a110e254
Carlo Bernucci Gómez <carlobernucci@gmail.com>
2022-10-09 17:18:07 -0300

    Update searchs.js

--- routes/searchs.js
+++ routes/searchs.js
@@ -5,7 +5,7 @@ const search = require('../services/search');
 /* General search */
 router.get('/', async function(req, res, next) {
   try {
-    res.json(await search.getSearch(req.query.title));
+    res.json(await search.getSearch(req.query.title,req.query.page));
   } catch (err) {
     console.error(`Error while getting anything `, err.message);
     next(err);
