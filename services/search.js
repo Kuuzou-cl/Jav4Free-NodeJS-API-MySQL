365d4f107de2707067c95770936c23617ab90f26
Carlo Bernucci Gómez <carlobernucci@gmail.com>
2022-10-08 21:48:51 -0300

    update search

--- services/search.js
+++ services/search.js
@@ -14,12 +14,22 @@ async function getSearch(title) {
     const rowsIdols = await db.query(stringIdols);
     const dataIdols = helper.emptyOrRows(rowsIdols);
 
+    /* Categories search */
+    var stringCategories;
+    var idCategories = '12';
+    words.forEach(async word => {
+        var tempQuery = `select id from Category WHERE name = '%${word}%'`;
+        const tempRow = await db.query(tempQuery);
+        idCategories = idCategories.concat(',',tempRow[0])
+    });
+    stringCategories= 'SELECT * from Scene s join SceneCategory sc on s.id = sc.sceneId where sc.categoryId in ()';
 
     return{
         searching,
         dataIdols,
-        stringIdols,
-        words
+        words,
+        stringCategories,
+        idCategories
     }
 }
 
