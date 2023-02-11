6c1bcb4b12a0169666bb901bf79c75e97686aacb
Carlo Bernucci Gómez <carlobernucci@gmail.com>
2023-02-11 17:47:51 -0300

    hotfix

--- services/scene.js
+++ services/scene.js
@@ -45,7 +45,7 @@ async function getScenes(limit = 1, order = 'desc') {
 
 async function getRelatedScenes(id = 1, limit = 1) {
     const rows = await db.query(
-        `SELECT s.id as Scene, (SELECT count(c1.id) FROM Category c1 JOIN SceneCategory sc1 ON c1.id = sc1.categoryId WHERE sc1.sceneId = s.id and c1.id
+        `SELECT s.*, (SELECT count(c1.id) FROM Category c1 JOIN SceneCategory sc1 ON c1.id = sc1.categoryId WHERE sc1.sceneId = s.id and c1.id
         IN 
         (SELECT c2.id FROM Category c2 JOIN SceneCategory sc2 ON c2.id = sc2.categoryId WHERE sc2.sceneId = ${id})) as matchCount 
         from Scene s where id <> ${id} order by matchCount desc limit ${limit}`
