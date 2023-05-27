21ba181405a078bd7876ab076c12192fe09c12e0
Carlo Bernucci Gómez <carlobernucci@gmail.com>
2023-05-27 16:07:20 -0400

    search query

--- routes/searchs.js
+++ routes/searchs.js
@@ -12,4 +12,14 @@ router.get('/', async function(req, res, next) {
   }
 });
 
+/* General search */
+router.get('/v2', async function(req, res, next) {
+  try {
+    res.json(await search.getSearchV2(req.query.title,req.query.page));
+  } catch (err) {
+    console.error(`Error while getting anything `, err.message);
+    next(err);
+  }
+});
+
 module.exports = router;
--- services/search.js
+++ services/search.js
@@ -17,7 +17,7 @@ async function getSearch(title, page = 1) {
 
     /* Scenes title or code search */
     var stringScenes = `SELECT * FROM Scene s where s.title like '%${searching}%' union SELECT * FROM Scene s WHERE s.code like '%${searching}%'`
-    
+
 
     /* Search per word */
     var stringIdCategories = 'SELECT s.id, s.title, s.code, s.video, s.duration, s.hide, s.previewImage, s.staticImage, s.creation, s.vtt, s.video480p from Scene s join SceneCategory sc on s.id = sc.sceneId where sc.categoryId in (0';
@@ -29,7 +29,7 @@ async function getSearch(title, page = 1) {
             }
         });
     });
-    stringIdCategories = stringIdCategories.concat('',')');
+    stringIdCategories = stringIdCategories.concat('', ')');
 
     var stringIdIdols = 'SELECT s.id, s.title, s.code, s.video, s.duration, s.hide, s.previewImage, s.staticImage, s.creation, s.vtt, s.video480p FROM Scene s join SceneIdol si on s.id = si.sceneId WHERE si.idolId in (0';
     const tempAllRowsIdols = await db.query(`select * from Idol`);
@@ -40,7 +40,7 @@ async function getSearch(title, page = 1) {
             }
         });
     });
-    stringIdIdols = stringIdIdols.concat('',')');
+    stringIdIdols = stringIdIdols.concat('', ')');
 
     var stringCode = `SELECT * from Scene s WHERE s.code like '%XXX%'`;
     words.forEach(word => {
@@ -48,10 +48,10 @@ async function getSearch(title, page = 1) {
         stringCode = stringCode.concat(' ', tempWord);
     });
 
-    stringScenes = stringScenes.concat(' union ',stringIdCategories);
-    stringScenes = stringScenes.concat(' union ',stringIdIdols);
-    stringScenes = stringScenes.concat(' union ',stringCode);
-    
+    stringScenes = stringScenes.concat(' union ', stringIdCategories);
+    stringScenes = stringScenes.concat(' union ', stringIdIdols);
+    stringScenes = stringScenes.concat(' union ', stringCode);
+
 
     const rowsScenes = await db.query(stringScenes);
     stringScenes = stringScenes.concat(' order by id ', `LIMIT ${offset},${config.listPerPageScenes}`)
@@ -59,9 +59,9 @@ async function getSearch(title, page = 1) {
     const dataScenesNoLimit = helper.emptyOrRows(rowsScenes);
     const dataScenes = helper.emptyOrRows(rowsScenesLimit);
 
-    const pagesData = helper.getCountPages(page,config.listPerPageScenes,dataScenesNoLimit.length);
+    const pagesData = helper.getCountPages(page, config.listPerPageScenes, dataScenesNoLimit.length);
 
-    const meta = { page : page, nextPage: pagesData.nextPage, lastPage: pagesData.lastPage };
+    const meta = { page: page, nextPage: pagesData.nextPage, lastPage: pagesData.lastPage };
 
     return {
         dataIdols,
@@ -70,6 +70,51 @@ async function getSearch(title, page = 1) {
     }
 }
 
+async function getSearchV2(title, page = 1) {
+    const offset = helper.getOffset(page, config.listPerPageScenes);
+    const splitted = title.split(' ');
+    let rows = [];
+
+    //Search if word is in title
+    const queryByTitle = 'SELECT s.* FROM Scene s WHERE s.title LIKE ?';
+    for (let index = 0; index < splitted.length; index++) {
+        let tempString = '%' + splitted[index] + '%';
+        let rowsByTitle = await db.query(queryByTitle, [tempString]);
+        for (let indexb = 0; indexb < helper.emptyOrRows(rowsByTitle).length; indexb++) {
+            if (!rows.some(item => item.id === rowsByTitle[indexb].id)) {
+                rows.push(rowsByTitle[indexb]);
+            }
+        }
+    }
+
+    //Search if word is a category
+    const queryByCategory = 'SELECT s.* FROM Scene s JOIN SceneCategory sc on s.id = sc.sceneId JOIN Category c on c.id = sc.categoryId WHERE c.name LIKE ?';
+    for (let index = 0; index < splitted.length; index++) {
+        let rowsByCategories = await db.query(queryByCategory, [splitted[index]]);
+        for (let indexb = 0; indexb < helper.emptyOrRows(rowsByCategories).length; indexb++) {
+            if (!rows.some(item => item.id === rowsByCategories[indexb].id)) {
+                rows.push(rowsByCategories[indexb]);
+            }
+        }
+    }
+
+    //Search if word is an idol
+    const queryByIdol = 'SELECT s.* from Scene s JOIN SceneIdol si on s.id = si.sceneId JOIN Idol i on i.id = si.idolId where i.name LIKE ?';
+    for (let index = 0; index < splitted.length; index++) {
+        let rowsByIdols = await db.query(queryByIdol, [splitted[index]]);
+        for (let indexb = 0; indexb < helper.emptyOrRows(rowsByIdols).length; indexb++) {
+            if (!rows.some(item => item.id === rowsByIdols[indexb].id)) {
+                rows.push(rowsByIdols[indexb]);
+            }
+        }
+    }
+
+    return {
+        Scenes : helper.emptyOrRows(rows)
+    }
+}
+
 module.exports = {
-    getSearch
+    getSearch,
+    getSearchV2
 }
