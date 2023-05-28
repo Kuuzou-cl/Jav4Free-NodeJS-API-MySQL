9d79c3caa2c34b505e76c01fba249e2f08c27fe3
Carlo Bernucci Gómez <carlobernucci@gmail.com>
2023-05-28 14:18:43 -0400

    await connection.end

--- routes/searchs.js
+++ routes/searchs.js
@@ -3,16 +3,6 @@ const router = express.Router();
 const search = require('../services/search');
 
 /* General search */
-router.get('/', async function(req, res, next) {
-  try {
-    res.json(await search.getSearch(req.query.title,req.query.page));
-  } catch (err) {
-    console.error(`Error while getting anything `, err.message);
-    next(err);
-  }
-});
-
-/* General search */
 router.get('/v2', async function(req, res, next) {
   try {
     res.json(await search.getSearchV2(req.query.title,req.query.page));
--- services/search.js
+++ services/search.js
@@ -2,76 +2,7 @@ const db = require('./db');
 const helper = require('../helper');
 const config = require('../config');
 
-async function getSearch(title, page = 1) {
-    const offset = helper.getOffset(page, config.listPerPageScenes);
-    const searching = title;
-    const words = title.split(' ');
-
-    /* Idols search */
-    var stringIdols = `select * from Idol i WHERE name like '%${searching}%' `;
-    words.forEach(word => {
-        stringIdols = stringIdols.concat(' ', `union select * from Idol i WHERE name like '%${word}%'`);
-    });
-    const rowsIdols = await db.query(stringIdols);
-    const dataIdols = helper.emptyOrRows(rowsIdols);
-
-    /* Scenes title or code search */
-    var stringScenes = `SELECT * FROM Scene s where s.title like '%${searching}%' union SELECT * FROM Scene s WHERE s.code like '%${searching}%'`
-
-
-    /* Search per word */
-    var stringIdCategories = 'SELECT s.id, s.title, s.code, s.video, s.duration, s.hide, s.previewImage, s.staticImage, s.creation, s.vtt, s.video480p from Scene s join SceneCategory sc on s.id = sc.sceneId where sc.categoryId in (0';
-    const tempAllRowsCategories = await db.query(`select * from Category`);
-    words.forEach(word => {
-        tempAllRowsCategories.forEach(tempCategory => {
-            if (tempCategory.name.toUpperCase() == word.toUpperCase()) {
-                stringIdCategories = stringIdCategories.concat(',', tempCategory.id);
-            }
-        });
-    });
-    stringIdCategories = stringIdCategories.concat('', ')');
-
-    var stringIdIdols = 'SELECT s.id, s.title, s.code, s.video, s.duration, s.hide, s.previewImage, s.staticImage, s.creation, s.vtt, s.video480p FROM Scene s join SceneIdol si on s.id = si.sceneId WHERE si.idolId in (0';
-    const tempAllRowsIdols = await db.query(`select * from Idol`);
-    words.forEach(word => {
-        tempAllRowsIdols.forEach(tempIdol => {
-            if (tempIdol.name.toUpperCase().includes(word.toUpperCase())) {
-                stringIdIdols = stringIdIdols.concat(',', tempIdol.id);
-            }
-        });
-    });
-    stringIdIdols = stringIdIdols.concat('', ')');
-
-    var stringCode = `SELECT * from Scene s WHERE s.code like '%XXX%'`;
-    words.forEach(word => {
-        var tempWord = `or s.code like '%${word}%'`;
-        stringCode = stringCode.concat(' ', tempWord);
-    });
-
-    stringScenes = stringScenes.concat(' union ', stringIdCategories);
-    stringScenes = stringScenes.concat(' union ', stringIdIdols);
-    stringScenes = stringScenes.concat(' union ', stringCode);
-
-
-    const rowsScenes = await db.query(stringScenes);
-    stringScenes = stringScenes.concat(' order by id ', `LIMIT ${offset},${config.listPerPageScenes}`)
-    const rowsScenesLimit = await db.query(stringScenes);
-    const dataScenesNoLimit = helper.emptyOrRows(rowsScenes);
-    const dataScenes = helper.emptyOrRows(rowsScenesLimit);
-
-    const pagesData = helper.getCountPages(page, config.listPerPageScenes, dataScenesNoLimit.length);
-
-    const meta = { page: page, nextPage: pagesData.nextPage, lastPage: pagesData.lastPage };
-
-    return {
-        dataIdols,
-        dataScenes,
-        meta
-    }
-}
-
 async function getSearchV2(title, page = 1) {
-    const offset = helper.getOffset(page, config.listPerPageScenes);
     const splitted = title.split(' ');
     let rows = [];
 
@@ -123,6 +54,5 @@ async function getSearchV2(title, page = 1) {
 }
 
 module.exports = {
-    getSearch,
     getSearchV2
 }
